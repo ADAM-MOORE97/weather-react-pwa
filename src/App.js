47a4136eb1980076f351179cde0441cab49a80f4
Adam <adammoore118@outlook.com>
2022-03-25 16:34:00 -0400

    styling, search bar,weather api call logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { getWeather } from "./api/getWeather";
 import './App.css'
 const App = () =>{
-
-
+    const [query, setQuery] = useState('')
+ 
+    const search = async (e) =>{
+        if(e.key === 'Enter'){
+            const data = await getWeather(query)
+ 
+            console.log(data);        }
+    }
+ 
     return(
-     <h1>Weather App PWA</h1>
+        <div className="main-container">
+            <input
+                type='text'
+                className="search"
+                placeholder="Search..."
+                value={query}
+                onChange={(e)=>setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+        </div>
     );
 }
-
+ 
 export default App;
--- src/api/getWeather.js
+++ src/api/getWeather.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+ 
 export const getWeather = async (query) =>{
     const {data} = await axios.get(process.env.local.REACT_APP_WEATHER_URL, {
         params: {
