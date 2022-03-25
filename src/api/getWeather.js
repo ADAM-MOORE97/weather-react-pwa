25d6f4d6966074e73d8fba3a2032b4251ab6c79e
Adam <adammoore118@outlook.com>
2022-03-25 16:30:28 -0400

    initial commit

---
+++ src/api/getWeather.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const getWeather = async (query) =>{
+    const {data} = await axios.get(process.env.local.REACT_APP_WEATHER_URL, {
+        params: {
+            q: query,
+            APPID: process.env.local.REACT_APP_WEATHER_API_KEY,
+        }
+    })
+    return data;
+};
+
